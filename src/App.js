da00a142f3d21e0cafb595801cbc45df590e947e
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-09 15:08:28 +0200

    remove the unnecessary folders and files which the project does not need it

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/nav/nav';
+import Home from './pages/Home';
+import Book from './pages/Book';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="book" element={<Book />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
