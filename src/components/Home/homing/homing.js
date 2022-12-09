7fabbf5cf229ed732661b4523400af27ec7b486e
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-09 17:48:54 +0200

    Add the singleBook component at the homing component

--- src/components/Home/homing/homing.js
+++ src/components/Home/homing/homing.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import SingleBook from '../singleBook/singleBook';
+import './homing.css';
 
 function Homing() {
+  const books = [
+    {
+      id: 1,
+      type: 'action',
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: 2,
+      type: 'action',
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: 3,
+      type: 'action',
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ];
   return (
-    <div>Homing</div>
+    <>
+      <section className="sectionone" id="sectionone">
+        <div className="container">
+          <div className="sectionone__container__contents">
+            {books.map((book) => (
+              <SingleBook
+                type={book.type}
+                title={book.title}
+                author={book.author}
+                key={book.id}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
