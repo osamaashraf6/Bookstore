7c5bfadc095067a20da92803d4d3dfe160b5384f
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-15 21:36:37 +0200

    Call the dispatch and useSelector for dispalying the data

--- src/components/Book/booking/booking.js
+++ src/components/Book/booking/booking.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { check } from '../../../redux/categories/categories';
+import getDataCategory from '../../../redux/categories/categories__api';
 import './booking.css';
 
 function Booking() {
   // call the state and the dispatch
   const dispatch = useDispatch();
   const status = useSelector((state) => state.categories);
+  useEffect(() => {
+    dispatch(getDataCategory());
+  }, [dispatch]);
   return (
     <>
-      <button id="catButton" type="submit" onClick={() => dispatch(check())}>Check Status</button>
-      <p>{status}</p>
+      {status.length > 0 ? (status.map((book) => <li key={book.item_id}>{book.category}</li>))
+        : (<p>No categories</p>)}
     </>
   );
 }
